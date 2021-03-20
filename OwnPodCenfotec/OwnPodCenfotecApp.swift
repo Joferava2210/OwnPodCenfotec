87bb98e623f8ba8dca8fa1f96e9f456d046134c4
joferava <jfelipe221095@gmail.com>
2021-03-20 17:22:55 -0600

    Initial Commit

---
+++ OwnPodCenfotec/OwnPodCenfotecApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnPodCenfotecApp.swift
+//  OwnPodCenfotec
+//
+//  Created by Felipe Ramirez Vargas on 20/3/21.
+//
+
+import SwiftUI
+
+@main
+struct OwnPodCenfotecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
